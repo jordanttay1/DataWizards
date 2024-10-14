5567c5afa948ac35cd844dab17154c01f08b7972
D'Onofrio, William <16698591+wdonofrio@users.noreply.github.com>
2024-10-13 23:20:50 -0600

    Convert to username instead of UID
    Not sure if we even want to deal with UID

--- network/main.py
+++ network/main.py
@@ -16,15 +16,17 @@ class PlayerNode:
 
 def add_node(graph: nx.Graph, node: PlayerNode) -> nx.Graph:
     """Add a node to the graph if it doesn't already exist."""
-    if node.uid not in graph:
+    if node.username not in graph:
         graph.add_node(node.username, **asdict(node))
     return graph
 
 
 def add_edge(graph: nx.Graph, node1: PlayerNode, node2: PlayerNode) -> nx.Graph:
     """Add an edge between two nodes if it doesn't already exist."""
-    graph = add_node(graph, node1)
-    graph = add_node(graph, node2)
-    if not graph.has_edge(node1.uid, node2.uid):
-        graph.add_edge(node1.uid, node2.uid)
+    if node1.username not in graph:
+        graph = add_node(graph, node1)
+    if node2.username not in graph:
+        graph = add_node(graph, node2)
+    if not graph.has_edge(node1.username, node2.username):
+        graph.add_edge(node1.username, node2.username)
     return graph
