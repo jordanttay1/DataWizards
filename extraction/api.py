c909c51701fd719a4edc2ed6870ec368762940f7
D'Onofrio, William <16698591+wdonofrio@users.noreply.github.com>
2024-10-28 23:37:52 -0600

    Add API retry
    If 429 is hit (rate limit), retry after some time in a loop. If another error. Stop.

--- extraction/api.py
+++ extraction/api.py
@@ -1,33 +1,59 @@
+import random
+import time
+from functools import wraps
+
 import chessdotcom as cdc
 
 from cache import cache
 
 
+def api_429_retry(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        while True:
+            try:
+                return func(*args, **kwargs)
+            except cdc.ChessDotComError as exc:
+                if exc.status_code == 429:
+                    print("Retrying after some seconds...")
+                    time.sleep(random.randint(1, 5))
+                    continue
+                else:
+                    raise
+
+    return wrapper
+
+
 @cache
+@api_429_retry
 def get_player_game_archives(username: str):
     """Get the game archives of a player."""
     return cdc.get_player_game_archives(username).json
 
 
 @cache
+@api_429_retry
 def get_player_games_by_month(username: str, year: int, month: int):
     """Get the games of a player by month."""
     return cdc.get_player_games_by_month(username, year, month).json
 
 
 @cache
+@api_429_retry
 def get_player_games_by_month_pgn(username: str, year: int, month: int):
     """Get the games of a player by month."""
     return cdc.get_player_games_by_month_pgn(username, year, month).json
 
 
 @cache
+@api_429_retry
 def get_player_profile(username: str):
     """Get the profile of a player."""
     return cdc.get_player_profile(username).json
 
 
 @cache
+@api_429_retry
 def get_player_stats(username: str):
     """Get the stats of a player."""
     return cdc.get_player_stats(username).json
